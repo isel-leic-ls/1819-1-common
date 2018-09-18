98460792f6e4d6b30f0064afc0d4c5c2a5d3aa14
fbfreitas <ffreitas@cc.isel.ipl.pt>
2018-09-18 12:10:53 +0100

    Phase 0

--- src/main/java/pt/isel/ls/Ints.java
+++ src/main/java/pt/isel/ls/Ints.java
@@ -6,4 +6,22 @@ public class Ints {
         return a >= b ? a : b;
     }
 
+    public static int indexOfBinary(int[] a, int fromIndex, int toIndex, int n) {
+
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException("from(" + fromIndex + ") > to(" + toIndex + ")");
+        }
+
+        int low = fromIndex;
+        int high = toIndex - 1;
+        int mid;
+
+        while(low < high) {
+            mid = high + low / 2 + 1;
+            if(n > a[mid]) low = mid + 1;
+            else if(n < a[mid]) high = mid - 1;
+            else return mid;
+        }
+        return -1;
+    }
 }
